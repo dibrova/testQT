a111dbf0aa20a73da79887b4f3c144948a8f5c62
dibrova <D:\The Bat!\Mail>
2017-12-26 19:49:21 +0200

    cosmetic commit

--- funcs.cpp
+++ funcs.cpp
@@ -8,19 +8,19 @@
 using namespace std;
 
 void readFromFile(string &str){
-	QFile inputFile("exemple.txt");
-        if (inputFile.open(QIODevice::ReadOnly)){
-           QTextStream in(&inputFile);
-           while (!in.atEnd()){
-              str = in.readLine().toStdString();
-           }
-           inputFile.close();
-        }
+    QFile inputFile("exemple.txt");
+    if (inputFile.open(QIODevice::ReadOnly)){
+       QTextStream in(&inputFile);
+       while (!in.atEnd()){
+          str = in.readLine().toStdString();
+       }
+       inputFile.close();
+    }
 }
 
 void writeToFile(string &str){
     QFile outputFile("exemple_out.txt");
-	if (outputFile.open(QIODevice::WriteOnly)) {
+    if (outputFile.open(QIODevice::WriteOnly)) {
         QTextStream out(&outputFile);
         out << QString::fromStdString(str);
         outputFile.flush();
@@ -37,59 +37,59 @@ string reverseStrToFile(string &str) {
         iss >> word;
         st.push(word);
     }
-	while (!st.empty()) {
+    while (!st.empty()) {
         buf += st.top() + " ";
         st.pop();
     }
-	buf.erase(buf.end() - 1, buf.end());
-	buf.erase(buf.begin(), buf.begin() + 1);
-	writeToFile(buf);
+    buf.erase(buf.end() - 1, buf.end());
+    buf.erase(buf.begin(), buf.begin() + 1);
+    writeToFile(buf);
     return buf;
 }
 
 string countRepeatCharToFile(string &str) {
-	list<string> buff;
-	uint max = 0;
-	for (uint i = 0; i < str.length(); i++) {
+        list<string> buff;
+        uint max = 0;
+        for (uint i = 0; i < str.length(); i++) {
         string tmp;
-		if (str[i] == ' ' || str[i + 1] == ' ') continue;
-		tmp += str[i];
-		tmp += str[i + 1];
-		auto n1 = str.find(tmp);
-		auto n2 = str.rfind(tmp);
-		if (n1 == n2) continue;
+        if (str[i] == ' ' || str[i + 1] == ' ') continue;
+        tmp += str[i];
+        tmp += str[i + 1];
+        auto n1 = str.find(tmp);
+        auto n2 = str.rfind(tmp);
+        if (n1 == n2) continue;
         uint cnt = 1;
-		while (n1 != n2) {			
-			n1 = str.find(tmp, n1 + 1);
-			cnt++;
+        while (n1 != n2) {
+            n1 = str.find(tmp, n1 + 1);
+            cnt++;
         }
-		if (cnt < max)continue;
-		auto check = count(buff.begin(), buff.end(), tmp);
-		if (cnt == max && check == 0)  buff.push_back(tmp);
-		if (cnt > max) {
-            buff.clear();
-            buff.push_back(tmp);
-			max = cnt;
+        if (cnt < max)continue;
+        auto check = count(buff.begin(), buff.end(), tmp);
+        if (cnt == max && check == 0)  buff.push_back(tmp);
+        if (cnt > max) {
+        buff.clear();
+        buff.push_back(tmp);
+        max = cnt;
         }
     }
     string tmp;
-	tmp += to_string(max) + ": ";
-	foreach(string s, buff) {
-		tmp += s + " ";
+        tmp += to_string(max) + ": ";
+        foreach(string s, buff) {
+        tmp += s + " ";
     }
     tmp.pop_back();
-	writeToFile(tmp);
+    writeToFile(tmp);
     return tmp;
 }
 
 string replacePoint(string &str){
-	for_each(str.begin(), str.end(), [](char &n) { if (n == ',') n = '.'; });
+    for_each(str.begin(), str.end(), [](char &n) { if (n == ',') n = '.'; });
     writeToFile(str);
     return str;
 }
 
 string removeSpaces(string &str) {
-	str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
     writeToFile(str);
     return str;
 }
@@ -98,23 +98,23 @@ string foundChar(string &str){
     char key;
     uint cnt;
     string res;
-	cout << "Please input char for count: ";
-	key = getchar();
-	cnt = count(str.begin(), str.end(), key);
-	res += "Found " + string(1, key) + " x " + to_string(cnt);
+    cout << "Please input char for count: ";
+    key = getchar();
+    cnt = count(str.begin(), str.end(), key);
+    res += "Found " + string(1, key) + " x " + to_string(cnt);
     writeToFile(res);
     return res;
 }
 
 string foundDigitToFile(string &str) {
     string buf;
-	foreach(char ch, str) {
-		if (isdigit(ch)) {
-			buf += ch;
-			buf += " ";
+    foreach(char ch, str) {
+        if (isdigit(ch)) {
+            buf += ch;
+            buf += " ";
         }
     }
-	if (buf.size())buf.pop_back();
+    if (buf.size()) buf.pop_back();
     writeToFile(buf);
     return buf;
 }
